5a3fe2e19110e8ebd478fbbc27d8d9ec6b533864
dulap16 <tudorbrk8@yahoo.com>
2024-05-07 17:57:29 +0300

    More of them

--- app/src/main/java/com/example/recipeai/MainActivity.java
+++ app/src/main/java/com/example/recipeai/MainActivity.java
@@ -17,9 +17,9 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
