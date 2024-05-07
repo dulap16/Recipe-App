85d26da6c000520527ac1177a2f03d655c86dfad
dulap16 <tudorbrk8@yahoo.com>
2024-05-07 17:57:19 +0300

    Necessary libraries

--- app/src/main/java/com/example/recipeai/MainActivity.java
+++ app/src/main/java/com/example/recipeai/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.recipeai;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
