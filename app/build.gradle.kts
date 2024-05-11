30047b19f83eb91ce0f350c04b914ab5d07f1e92
dulap16 <tudorbrk8@yahoo.com>
2024-05-12 02:31:10 +0300

    Change version of play-services-vision

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation("com.google.android.gms:play-services-vision:19.0.0")
+    implementation("com.google.android.gms:play-services-vision:19.0.0+")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
