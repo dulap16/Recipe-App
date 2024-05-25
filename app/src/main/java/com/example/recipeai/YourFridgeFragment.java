d4fab4eaeec12a830adeb3c32788759bfd9461b5
teobosie28 <tudorbrk8@yahoo.com>
2024-05-25 17:48:03 +0300

    Finish YourFridgeFragment

--- app/src/main/java/com/example/recipeai/YourFridgeFragment.java
+++ app/src/main/java/com/example/recipeai/YourFridgeFragment.java
@@ -4,44 +4,30 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link YourFridgeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class YourFridgeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<Food> availableIngredients;
+    private ListView ingredientList;
+    private EditText newIngredientEditText;
+    private Button newIngredientButton;
 
     public YourFridgeFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment YourFridgeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YourFridgeFragment newInstance(String param1, String param2) {
+    public static YourFridgeFragment newInstance(ArrayList<String> param1) {
         YourFridgeFragment fragment = new YourFridgeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putStringArrayList("ingredients", param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +35,83 @@ public class YourFridgeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        availableIngredients = stringToFoodList(getArguments().getStringArrayList("ingredients"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_your_fridge, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ingredientList = view.findViewById(R.id.ingredient_list);
+        newIngredientEditText = view.findViewById(R.id.newIngredientEditText);
+        newIngredientButton = view.findViewById(R.id.newIngredientButton);
+        updateList();
+
+        newIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(newIngredientEditText.getText().toString() != "") {
+                    addIngredient(newIngredientEditText.getText().toString());
+                    newIngredientEditText.setText("");
+                    updateList();
+                }
+            }
+        });
+    }
+
+    public void setAvailableIngredients(ArrayList<Food> availableIngredients) {
+        this.availableIngredients = availableIngredients;
+    }
+
+    public void addIngredient(String name) {
+        if(name == "")
+            return ;
+
+        availableIngredients.add(new Food(name));
+
+        ((MainActivity)getActivity()).addIngredient(name);
+    }
+
+    public void addIngredient(Food f) {
+        availableIngredients.add(f);
+
+        ((MainActivity)getActivity()).addIngredient(f.getName());
+    }
+
+    public void removeIngredient(String name) {
+        availableIngredients.remove(name);
+
+        ((MainActivity)getActivity()).removeIngredient(name);
+    }
+
+    public void removeIngredient(Food f) {
+        availableIngredients.remove(f);
+
+        ((MainActivity)getActivity()).removeIngredient(f.getName());
+    }
+
+    private ArrayList<Food> stringToFoodList(ArrayList<String> stringList) {
+        ArrayList<Food> foodList = new ArrayList<>();
+        for (String s : stringList) {
+            foodList.add(new Food(s));
+        }
+        return foodList;
+    }
+
+    public void updateList() {
+        ItemAdapter adapter = new ItemAdapter(getContext(), availableIngredients);
+        ingredientList.setAdapter(adapter);
+    }
+
+    public void test(String text) {
+        Log.i("wtferror", text);
+    }
+
 }
